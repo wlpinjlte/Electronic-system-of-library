001d514c0449072e40c5fc3804137945d8b567a4
admin-Komputer\admin <wagulec123@gmail.com>
2023-04-07 21:59:20 +0200

    created server and connection to mongodb

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express =  require('express')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const bodyPraser = require('body-parser')
+
+const cors=require("cors")
+
+mongoose.connect('mongodb+srv://admin:admin@kurs.hwy4owx.mongodb.net/?retryWrites=true&w=majority')
+const db= mongoose.connection
+
+db.on('error',(err)=>{
+    console.log(err)
+})
+
+db.once('open',()=>{
+    console.log("connect dataBase")
+})
+
+
+const app=express()
+app.use(cors())
+
+app.use(morgan('dev'))
+app.use(bodyPraser.urlencoded({extended:true}))
+app.use(bodyPraser.json())
+
+
+const PORT = process.env.port || 3000
+app.listen(PORT,()=>{
+    console.log(`server running on port ${PORT}`)
+})
